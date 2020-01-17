54a9b7cbbeff19b9c9afe67b06c7a4a9def0fca2
Indri Mahadiraka <indri.mahadiraka@gmail.com>
2020-01-17 18:09:10 +0700

    exercise 6 is solved

--- python_foundation/exercise6.py
+++ python_foundation/exercise6.py
@@ -1,2 +1,14 @@
 ### Write a function that takes year(integer) as an input and compute
 ### whether the given year is leap year or not
+
+def exercise6(a):
+    if a%4 ==0:
+        if a%100 ==0:
+            if a%400 ==0:
+                return True
+            else:
+                return False
+        else:
+            return True            
+    else:
+        return False
